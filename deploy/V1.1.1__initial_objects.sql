c505e3fdc72ca69e5d20d9e585bbf1f4086e1039
kaavyanaik <kaavya.gn@gmail.com>
2026-09-20 23:41:23 +0530

    Fix employee insert SQL

--- deploy/V1.1.1__initial_objects.sql
+++ deploy/V1.1.1__initial_objects.sql
@@ -18,40 +18,31 @@ CREATE TABLE IF NOT EXISTS hrdata.employees
     PRIMARY KEY (employee_id)
 );
 
--- Insert data into tables
+-- Insert data into employees
 
 INSERT INTO hrdata.employees VALUES
 (100, 'Rama', 'Rao', 'RRAO', '420.271.4549',
- TO_DATE('17-JUN-1987', 'dd-MON-yyyy'), 'ACCT_PRES', 24000, NULL, NULL, 90);
-
+ TO_DATE('17-JUN-1987', 'dd-MON-yyyy'), 'ACCT_PRES', 24000, NULL, NULL, 90),
 (101, 'Devi', 'Kapoor', 'DKAPOOR', '420.271.4568',
- TO_DATE('21-SEP-1989', 'dd-MON-yyyy'), 'ACCT_VP', 17000, NULL, 100, 90);
-
+ TO_DATE('21-SEP-1989', 'dd-MON-yyyy'), 'ACCT_VP', 17000, NULL, 100, 90),
 (102, 'Nagesh', 'Reddy', 'NREDDY', '420.271.4549',
- TO_DATE('13-JAN-1993', 'dd-MON-yyyy'), 'ACCT_VP', 17000, NULL, 100, 90);
-
+ TO_DATE('13-JAN-1993', 'dd-MON-yyyy'), 'ACCT_VP', 17000, NULL, 100, 90),
 (103, 'Ranga', 'Rayudu', 'RRANGA', '590.423.4549',
- TO_DATE('03-JUL-1990', 'dd-MON-yyyy'), 'IT_PROG', 9000, NULL, 102, 60);
-
+ TO_DATE('03-JUL-1990', 'dd-MON-yyyy'), 'IT_PROG', 9000, NULL, 102, 60),
 (104, 'Sivarama', 'Krishna', 'SOKRISHNA', '590.423.4568',
- TO_DATE('21-MAY-1991', 'dd-MON-yyyy'), 'IT_PROG', 6000, NULL, 103, 60);
-
+ TO_DATE('21-MAY-1991', 'dd-MON-yyyy'), 'IT_PROG', 6000, NULL, 103, 60),
 (105, 'Shoban', 'Reddy', 'RSHOBAN', '590.423.4549',
- TO_DATE('25-JUN-1992', 'dd-MON-yyyy'), 'IT_PROG', 4800, NULL, 103, 60);
-
+ TO_DATE('25-JUN-1992', 'dd-MON-yyyy'), 'IT_PROG', 4800, NULL, 103, 60),
 (106, 'Jamuna', 'Kumari', 'JKAPURI', '590.423.4540',
- TO_DATE('05-FEB-1993', 'dd-MON-yyyy'), 'IT_PROG', 4800, NULL, 103, 60);
-
+ TO_DATE('05-FEB-1993', 'dd-MON-yyyy'), 'IT_PROG', 4800, NULL, 103, 60),
 (107, 'Jaya', 'Prakash', 'JPRAK', '590.423.5557',
- TO_DATE('07-FEB-1990', 'dd-MON-yyyy'), 'IT_PROG', 4200, NULL, 103, 60);
-
+ TO_DATE('07-FEB-1990', 'dd-MON-yyyy'), 'IT_PROG', 4200, NULL, 103, 60),
 (108, 'Raja', 'Krishna', 'RKRISHNA', '613.234.4599',
- TO_DATE('17-AUG-1994', 'dd-MON-yyyy'), 'GL_MGR', 12000, NULL, 101, 100);
-
+ TO_DATE('17-AUG-1994', 'dd-MON-yyyy'), 'GL_MGR', 12000, NULL, 101, 100),
 (109, 'Mohan', 'Chandra', 'MOCHANDRA', '613.234.4169',
  TO_DATE('16-AUG-1994', 'dd-MON-yyyy'), 'GL_ACCOUNT', 9000, NULL, 108, 100);
 
- -- Insert data into tables
+-- Insert data into departments
 
 INSERT INTO hrdata.departments VALUES
 (10, 'Administration', 200, 1700),
